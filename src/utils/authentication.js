3a0343ad35512a9dfc6c80dc8bb7b23f2ace690c
Amin Ansari <aminansari135790@gmail.com>
2024-08-02 17:52:08 +0530

    impelement login and sing up module, implement validation and authentication using firebase

---
+++ src/utils/authentication.js
@@ -0,0 +1,32 @@
+import { auth } from "./firebase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+export const createUser = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            console.log(user);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            
+        });
+
+}
+
+export const logInUser = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+}
+
+
